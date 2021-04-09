a424292365ecbf5967dcd25951d574c987cdb903
Dave Jones <sect62@gmail.com>
2021-04-09 14:52:35 -0500

    Update secrets-example.ps1

--- secrets-example.ps1
+++ secrets-example.ps1
@@ -1,5 +1,5 @@
 # podcastindex API keys stored here for safe keeping
 
-$SCRIPT:UserAgent = 'podcastindex PowerShell proof of concept v1.0'
+$SCRIPT:UserAgent = 'powershell-podcastindex-org-example/v1.0'
 $SCRIPT:ApiKey = '0123456789ABCDEF0123'
 $SCRIPT:ApiSecret = '0123456789abcdef0123456789abcdef01234567'
